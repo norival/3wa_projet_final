6f124b8a667a3094ca98660dd0ab5ed4eb5d67ab
Xavier Laviron <xavier.laviron@gmx.fr>
2020-02-04 13:49:45 +0100

    adds ContentController methods for admin

--- src/Controller/ContentController.php
+++ src/Controller/ContentController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Content;
+use App\Form\ContentType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -57,5 +59,40 @@ class ContentController extends AbstractController
             'content' => $content,
             'route'   => $route,
         ]);
+    }
+
+    /**
+     * @Route("/{route}", name="content_update", methods={"POST"})
+     *
+     * @param  Request $request
+     * @param  string $route
+     * @return Response
+     */
+    public function update(Request $request, string $route)
+    {
+        $content = $this->em->getRepository(Content::class)->findOneBy(['route' => $route]);
+        $form = $this->createForm(ContentType::class, $content);
+
+        \dump($content);
+    }
+
+
+    /**
+     * @Route("/admin/list-content", name="content_list", methods={"GET"})
+     *
+     * @return JsonResponse
+     */
+    public function list()
+    {
+        $contents = $this->em->getRepository(Content::class)->findAll();
+
+        foreach ($contents as $content) {
+            $titles[] = [
+                'title' => $content->getTitle(),
+                'route' => $content->getRoute(),
+            ];
+        }
+
+        return new JsonResponse($titles);
     }
 }
