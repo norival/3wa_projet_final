8e777446edd128dacd06c54dc641613d0d069643
Xavier Laviron <xavier.laviron@gmx.fr>
2020-02-18 10:13:06 +0100

    adds ViewType form and template

---
+++ src/Form/ViewType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\View;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ViewType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class)
+            ->add('title', TextType::class)
+            ->add('content_type', CollectionType::class, [
+                'entry_type' => TextType::class,
+                'entry_options' => [
+                    'label' => false,
+                ],
+            ])
+            ->add('created_at')
+            ->add('updated_at')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => View::class,
+        ]);
+    }
+}
