cb10df0098a6f93681e7b57edf2ccfbafb198a96
Xavier Laviron <xavier.laviron@gmx.fr>
2020-02-25 07:50:05 +0100

    update View Fixtures

--- src/DataFixtures/ViewFixtures.php
+++ src/DataFixtures/ViewFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use App\Entity\View;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
@@ -10,18 +11,13 @@ class ViewFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
         $view = new View();
+        $user = $manager->getRepository(User::class)->findOneBy(['email' => 'test@test.com']);
+
+        $view->setUser($user);
         $view->setName('cv');
         $view->setTitle('Xavier Laviron');
         $view->setCreatedAt(date_create());
-        $view->setContentType([
-            'cv_header',
-            'skill',
-            'training',
-            'realisation',
-            'experience',
-        ]);
         $manager->persist($view);
 
         $manager->flush();
